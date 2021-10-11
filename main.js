b956bbec7b2ca2bc5b1d67afcdccd137745605ae
Hendfawaz <Hend.fawaz.ruh.js@tuwaiq.edu.sa>
2021-10-11 14:10:40 +0300

    Arryeshw

--- main.js
+++ main.js
@@ -28,3 +28,51 @@ const characters = [
         gender: 'male',
     },
 ];
+
+
+let Numbers = [23,54,32,87,47];
+let sum = Numbers.reduce(
+( total, currentValue ) => (total += currentValue),0);
+ console.log (sum);
+
+let Numbers2 = [16,4,2,0,19,6];
+console.log(Math.max(16,4,2,0,19,6));
+let Numbers3 = [1,2,3,4,5,6,7,8,9,10];
+var theLegth=Numbers3.length
+var newArray=[]
+for (var i=0;i< theLegth;i++){
+   let theValuePopped= Numbers3.pop()
+   newArray.push(theValuePopped)
+}
+console.log(newArray)
+
+let names = characters.map( (element)=>  element.name   )
+console.log (names)
+let height = characters.map( (element)=>  element.height   )
+console.log (height)
+
+
+
+let fliter =characters.filter( (el)=> el.mass<100);
+console.log(fliter);
+
+let fliter2 =characters.filter( (el)=> el.female);
+console.log(fliter2);
+
+function XX(characters) {
+   return characters.mass<50
+}
+characters.map(getNam2).sort();
+function getNam2(characters){
+   return characters.name
+}
+let eyes=characters.some((el)=>(el.eye_color=="blue"));
+console.log(eyes);
+let male=characters.some((el)=>(el.eye_color=="male"));
+console.log(male);
+
+let mass3=characters.every((el)=>(el.mass >40));
+console.log(mass);
+
+let height4=characters.every((el)=>(el.height<200));
+console.log(height);
