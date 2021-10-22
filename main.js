d3dd8174f24b017707fb0be2eaff78a83842d393
Hendfawaz <Hend.fawaz.ruh.js@tuwaiq.edu.sa>
2021-10-22 20:18:36 +0300

    Arrayes

--- main.js
+++ main.js
@@ -1,99 +1,144 @@
+// FOR LOOP
+// Numbers' summations
+let Numbers = [23,54,32,87,47]
+let sum =0;
+for (let i=0; i < Numbers.length ; i++) {
+sum = sum + Numbers[i];
+}
+console.log(sum)
+​
+// Maximum number
+let arry = [16,4,2,0,19,6]
+let largest = 0;
+​
+for (let i = 0; i < arry.length; i++) {
+if (arry[i] > largest) {
+    largest = arry[i];
+}
+}
+console.log(largest)
+​
+// Reverse Array  
+let arry1 =  [1,2,3,4,5,6,7,8,9,10]
+for (let i = arry1.length-1; i >= 0; i--) {  
+    console.log(arry1[i] + " ");  
+}   
+​
+//NESTED FOR LOOP 
+//stars pattern
+let myVar = 5;
+let string = " ";
+for (let i = 0; i < myVar; i++) {
+  for (let j = 0; j <= i; j++) {
+    string += "*";
+  }
+  string += "<br>";
+}
+document.write(string);
+​
+//While loop
+let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let i = 0;
+while (i < nums.length) {
+   let x = 2;
+   let prime = true;
+    while (x < nums[i]) {
+        if (nums[i] % x == 0) {
+            prime = false;
+            break;
+        }
+        x++;
+    }
+    if(prime) console.log(nums[i], "is prime ")
+    i++;
+}
+​
+​
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+{
+    name: 'Luke Skywalker',
+    height: 172,
+    mass: 77,
+    eye_color: 'blue',
+    gender: 'male',
+},
+{
+    name: 'Darth Vader',
+    height: 202,
+    mass: 136,
+    eye_color: 'yellow',
+    gender: 'male',
+},
+{
+    name: 'Leia Organa',
+    height: 150,
+    mass: 49,
+    eye_color: 'brown',
+    gender: 'female',
+},
+{
+    name: 'Anakin Skywalker',
+    height: 188,
+    mass: 84,
+    eye_color: 'blue',
+    gender: 'male',
+},
 ];
-// For Loop
-
-let Numbers = [23,54,32,87,47];
-let sum = Numbers.reduce(
-( total, currentValue ) => (total += currentValue),0);
- console.log (sum);
-
-let Numbers2 = [16,4,2,0,19,6];
-console.log(Math.max(16,4,2,0,19,6));
-let Numbers3 = [1,2,3,4,5,6,7,8,9,10];
-var theLegth=Numbers3.length
-var newArray=[]
-for (var i=0;i< theLegth;i++){
-   let theValuePopped= Numbers3.pop()
-   newArray.push(theValuePopped)
+​
+​
+​
+​
+// ARRAY
+// map
+const namesheight = characters.map(characters =>  ({text: characters.name , value: characters.height }))
+console.log(namesheight)
+​
+const fnames = characters.map(x => x.name)
+console.log(fnames)
+​
+// // reduce
+const masses = characters.map(x=> x.mass) 
+const totalMasses = masses.reduce((prev,curr)=>prev+curr,0)
+console.log(totalMasses)
+​
+function characterCount(arr) {
+return arr.reduce(function(charCount, item) {
+  return charCount + item.length;
+}, 0); 
 }
-
-console.log(newArray)
-
-
-// ArrayMap
-
-let names = characters.map( (element)=>  element.name   )
-console.log (names)
-let height = characters.map( (element)=>  element.height   )
-console.log (height)
-
-
-// ArrayFilter
-let fliter =characters.filter( (el)=> el.mass<100);
-console.log(fliter);
-
-let fliter2 =characters.filter( (el)=> el.female);
-console.log(fliter2);
-
-function XX(characters) {
-   return characters.mass<50
-}
-// ArraySort
-characters.map(getNam2).sort();
-function getNam2(characters){
-   return characters.name
-}
+​
+console.log(characterCount(['Luke Skywalker', 'blue', 'male', 'male','yellow','Darth Vader','Leia Organa','brown','female','Anakin Skywalker','blue','male',]));
+​
+​
+//   FILTER
+const grater = characters.filter(character => character.mass < 200);
+console.log(grater)
+​
+const f = characters.filter(character => character.gender ==='female');
+console.log(f)
+​
+//SORT
+const sortNames = fnames.sort() 
+console.log(sortNames)
+​
 const genders = characters.map(x => x.gender)
 const sortGender = genders.sort() 
 console.log(sortGender)
-
-const f = characters.filter(character => character.gender ==='female');
-console.log(f)
-
-
-// ArrayEvery-Some-Sort
-let eyes=characters.some((el)=>(el.eye_color=="blue"));
-console.log(eyes);
-let male=characters.some((el)=>(el.eye_color=="male"));
-console.log(male);
-
-let mass3=characters.every((el)=>(el.mass >40));
-console.log(mass);
-
-let height4=characters.every((el)=>(el.height<200));
-console.log(height);
-
-const sortNames = fname.sort()
-console.log(sortNames)
-
-const sortGender = genders.sort() 
-console.log(sortGender)
-
+​
+// every
+const blueEyes = characters.every(function checkAge(character) {
+return character.eye_color ==='blue';
+})
+console.log(blueEyes)
+​
+const G = characters.every(function checkAge(character) {
+return character.gender ==='male';
+})
+console.log(G)
+​
+//   some
+const male1 = characters.some((b)=> b.gender ==='male')
+console.log(male1)
+​
+const mass2 = characters.some((x)=> x.mass<50)
+console.log(mass2)
